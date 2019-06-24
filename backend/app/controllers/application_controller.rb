5868c637b0f9b452e0994191895c7147e3a07747
Dwake5 <wakeling101@hotmail.com>
2019-06-24 15:05:35 +0100

    done

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+    
+    def current_user
+        id = decode_token['id']
+        User.find_by(id: id)
+    end
+
+    def decode_token
+        begin
+            JWT.decode(token, secret)
+        rescue
+            {}
+        end
+    end
+
+    def token
+        request.headers['Authorisation']
+    end
+
+    def issue_token(data)
+        JWT.encode(data, secret)
+    end
+
+
+    def secret
+        'random secret'
+    end
 end
